d6df62c7cce276c2b5ab1af40f0c298991704a56
Artem L <artuon@gmail.com>
2020-10-05 22:25:59 +0300

    Developing JQuery

--- js_basics/js/jquery.js
+++ js_basics/js/jquery.js
@@ -1,2 +1,16 @@
 /* --- */
-echo ('First');
+echo ('$ selectors');
+console.log($('#the-second'));
+console.log($('li'));
+console.log($('ul.ul-circle li'));
+console.log($('ul.ul-circle li')[0]);
+
+/* --- */
+echo ('Style Changing',"'css' method");
+$('#the-second').css('color', '#ffbe0b')
+
+$('.paragraphs p').css({
+  border: '2px solid #ff006e',
+  fontSize: '20x',
+  padding: '10px'
+})
