3de22a68a9d4b94b0d98a572157b7073280a99f1
Artem L <artuon@gmail.com>
2020-10-06 22:30:26 +0300

    Developing JQuery: creating a mini app

--- js_basics/js/jquery.js
+++ js_basics/js/jquery.js
@@ -21,8 +21,60 @@ console.log($('h1').text(''));
 $('h1').text('JQuery Basics');
 
 let thirdLi = 'ul.ul-circle li:nth-child(3)';
-$(thirdLi).html('List 2 Item 3 <a href="https://v2.com" target="_blank">v2</a>');
+$(thirdLi).html('<span>List 2 Item 3 <a href="https://v2.com" target="_blank">v2</a></span>');
 $(thirdLi+' a').attr('id','link-v2');
 
 console.log($('#fname').val());
 console.log($('input[type="radio"]').val());
+
+
+/* --- */
+echo ('Class manipulating',"letItRun()");
+
+/* --- */
+echo ('Events',"letItRun()");
+
+//This is an mini app
+(function letItRun() { //just named wrapper, for reference
+  const itemName = '<span>List 2 Item</span> ';
+  let itemCount = 0;
+  const secondUl = 'ul.ul-circle';
+  const secondUlLis = 'ul.ul-circle li';
+  const secondUlLisContent = 'ul.ul-circle li span';
+
+  $('#add').on('click', function (){
+    itemCount = $(secondUlLis).length+1;
+    console.log(itemCount);
+    $(secondUl).append('<li>' + itemName + itemCount + '</li>');
+    $('#remove').attr('disabled', false);
+  })
+
+  $('#remove').on('click', function (){
+    itemCount = $(secondUlLis).length-1;
+    console.log(itemCount);
+    $(secondUlLis).last().remove();
+    if (itemCount<=0) {
+      $('#remove').attr('disabled', true);
+    }
+  })
+
+/*  $(secondUlLis).on('mouseover', 'span',function (){
+    $(this).addClass('li-hovered');
+    console.log($(this).html());
+    //$(this).html($(this).html() + ' ' + '<a href="#remove" class="link-remove">Remove</a>');
+  })*/
+
+
+  $(secondUlLis).on('mouseover', 'span',function (){
+    $(this).addClass('span-hovered');
+    console.log($(this).parent().html());
+  })
+  $(secondUlLis).on('mouseout', 'span',function (){
+    $(this).removeClass('span-hovered');
+  })
+
+  $(secondUl).on('click', 'li',function (){
+    $(this).toggleClass('span-selected');
+  })
+})();
+
