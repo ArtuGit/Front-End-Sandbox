108e2e2134d11eff508e7c13487e34c638e82fb0
Artem L <artuon@gmail.com>
2020-10-06 16:37:02 +0300

    Developing JQuery

--- js_basics/js/jquery.js
+++ js_basics/js/jquery.js
@@ -14,3 +14,15 @@ $('.paragraphs p').css({
   fontSize: '20x',
   padding: '10px'
 })
+
+/* --- */
+echo ('Methods',"'text','html','attr', 'val'");
+console.log($('h1').text(''));
+$('h1').text('JQuery Basics');
+
+let thirdLi = 'ul.ul-circle li:nth-child(3)';
+$(thirdLi).html('List 2 Item 3 <a href="https://v2.com" target="_blank">v2</a>');
+$(thirdLi+' a').attr('id','link-v2');
+
+console.log($('#fname').val());
+console.log($('input[type="radio"]').val());
