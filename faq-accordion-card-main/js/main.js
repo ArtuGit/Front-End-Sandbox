7603041e3d807282df82c2387a008b5763a311e0
Artem Laptiev <artuon@gmail.com>
2020-10-26 20:45:16 +0200

    Add transition

--- faq-accordion-card-main/js/main.js
+++ faq-accordion-card-main/js/main.js
@@ -1,15 +1,7 @@
 let acc = document.getElementsByClassName("qa-accordion");
 
-console.log(acc);
-
 for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
     this.classList.toggle("open");
-    /*    var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-          panel.style.maxHeight = null;
-        } else {
-          panel.style.maxHeight = panel.scrollHeight + "px";
-        }*/
   });
 }
