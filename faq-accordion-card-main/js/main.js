4de76e96c1e24656c552a3aadbc35ab8dd1af84f
Artem L <artuon@gmail.com>
2020-10-30 00:46:22 +0200

    Accessibility

--- faq-accordion-card-main/js/main.js
+++ faq-accordion-card-main/js/main.js
@@ -1,7 +1,21 @@
-let acc = document.getElementsByClassName("qa-accordion");
+// Handle accordion clicks
 
-for (let i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
+let accordions = document.getElementsByClassName("qa-accordion");
+
+for (let i = 0; i < accordions.length; i++) {
+  accordions[i].addEventListener("click", function () {
     this.classList.toggle("open");
   });
 }
+
+// Handle keys pressing on questions
+
+let questions = document.querySelectorAll("div.question"); // for keyboard
+for (let i = 0; i < questions.length; i++) {
+  questions[i].addEventListener("keyup", function (event) {
+    if (event.key === " " || event.key === "Enter" || event.key === "Spacebar") { // "Spacebar" for IE11 support
+      questions[i].parentElement.classList.toggle('open');
+    }
+  });
+}
+
