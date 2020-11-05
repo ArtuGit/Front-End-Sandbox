2dc076140e1de67f793cc0ea554c83962b1d9dbe
Artem Laptiev <artuon@gmail.com>
2020-11-05 17:22:44 +0200

    TailWind Sandbox

---
+++ tailwind-sandbox/postcss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
+      content: [
+        'app/*.html',
+        'app/css/*.css'
+      ]
+    })
+  ]
+}
+
