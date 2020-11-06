9e2c589d9ebe4f81f53b6a57164579422d9dcc39
Artem L <artuon@gmail.com>
2020-11-06 19:46:24 +0200

    FlexBox Demo: Refactor to have Hamburger Menu and Logo

---
+++ flexbox/js/main.js
@@ -0,0 +1,6 @@
+// Not required!
+// This is just to demo functionality.
+
+$("#add").on("click", function () {
+  $("<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus adipisci autem commodi eaque earum error facere iste itaque magni maxime minima nobis nulla quam quia quo quod reiciendis sint, totam voluptatem. A, accusantium, ad atque deleniti enim ex iste libero mollitia natus nesciunt nihil quas saepe suscipit, tenetur voluptates.</p>").appendTo(".content");
+});
