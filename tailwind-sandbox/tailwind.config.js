a907e1ca70e9b8d4adaa3499a8a0311e6048a5c6
Artem L <artuon@gmail.com>
2020-11-07 14:34:25 +0200

    FlexBox Demo: Developing

--- tailwind-sandbox/tailwind.config.js
+++ tailwind-sandbox/tailwind.config.js
@@ -9,16 +9,26 @@ module.exports = {
       'xs': '1px',
       'sm': '640px',
       // => @media (min-width: 640px) { ... }
-
       'md': '768px',
       // => @media (min-width: 768px) { ... }
-
       'lg': '1024px',
       // => @media (min-width: 1024px) { ... }
-
       'xl': '1280px',
       // => @media (min-width: 1280px) { ... }
-    }
+    },
+    extend: {
+      colors: {
+        'cst-dark': {
+          lighter: '#3d5a80',
+          default: '#293241',
+        },
+        'cst-light': {
+          darker: '#98c1d9',
+          default: '#e0fbfc',
+        },
+        'cst-orange': '#ee6c4d',
+      },
+    },
   },
   variants: {},
   plugins: [],
